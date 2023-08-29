f1496f37cf0016af61165f3dfff9e2afb08f118e
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-08-29 15:10:14 -0300

    realizando testes com Exceptions

---
+++ src/test/java/com/github/CaioPfaltzgraff/junit/ExceptionsTeste.java
@@ -0,0 +1,19 @@
+package com.github.CaioPfaltzgraff.junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExceptionsTeste {
+
+    @Test
+    void validaCenarioDeExcecaoNaTransferencia() {
+        Conta contaOrigem = new Conta("123456", 0);
+        Conta contaDestino = new Conta("456789", 100);
+
+        TransferenciaEntreContas transferenciaEntreContas = new TransferenciaEntreContas();
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> transferenciaEntreContas.transfere(contaOrigem, contaDestino, -1));
+        Assertions.assertDoesNotThrow(() -> transferenciaEntreContas.transfere(contaOrigem, contaDestino, 10));
+    }
+
+}
